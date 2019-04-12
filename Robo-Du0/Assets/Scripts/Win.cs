49f057c4f95b2548f3a613058bc6ba18b720d16c
Logan Davis <ldav113@lsu.edu>
2019-04-12 02:43:37 -0500

    Level Exit and More Animation
    Added an object so that the player can move on to the next level. Also added diagonal walking animations

---
+++ Robo-Du0/Assets/Scripts/Win.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+    public string SceneName;
+    public void Menu() // This MUST be inserted otherwise the script doesn't work, also the name of the scene to be loaded has to be where it says NewLebel (delete NewLebel and replace it with  your scene name
+    {
+        Debug.Log("Menu");
+        SceneManager.LoadScene("Menu");
+    }
+    void OnTriggerEnter2D(Collider2D co)
+    {
+        //this finds the game object called "Ball" in the scene
+        if (co.name == "Player") 
+            Debug.Log("Menu");
+        SceneManager.LoadScene(SceneName); 
+    }
+}
