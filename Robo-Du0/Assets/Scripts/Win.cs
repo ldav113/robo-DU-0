886a5780b1ec51c9435451150161cb3305aa74d4
Fettah <ftth05@gmail.com>
2019-04-20 18:22:03 -0500

    lava
    lava

--- Robo-Du0/Assets/Scripts/Win.cs
+++ Robo-Du0/Assets/Scripts/Win.cs
@@ -8,14 +8,14 @@ public class Win : MonoBehaviour
     public string SceneName;
     public void Menu() // This MUST be inserted otherwise the script doesn't work, also the name of the scene to be loaded has to be where it says NewLebel (delete NewLebel and replace it with  your scene name
     {
-        Debug.Log("Menu");
-        SceneManager.LoadScene("Menu");
+        Debug.Log("Level2Boss");
+        SceneManager.LoadScene("Level2Boss");
     }
     void OnTriggerEnter2D(Collider2D co)
     {
         //this finds the game object called "Ball" in the scene
         if (co.name == "Player") 
-            Debug.Log("Menu");
+            Debug.Log("Level2Boss");
         SceneManager.LoadScene(SceneName); 
     }
 }
